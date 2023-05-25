1e36c92b47307cb4e9e21d402f974366af0895a7
mutesialine <ktesialine@gmail.com>
2023-05-25 11:21:27 +0200

    refactor:use reduce method and add comments

--- romanNumeralsDecoder/index.js
+++ romanNumeralsDecoder/index.js
@@ -1,3 +1,9 @@
+/**
+ * Converts a Roman numeral to its decimal value.
+ * @param {string} roman - The Roman numeral to convert.
+ * @returns {number} - The decimal value of the Roman numeral.
+ */
+
 const romanNumeral = {
   I: 1,
   V: 5,
@@ -8,21 +14,19 @@ const romanNumeral = {
   M: 1000,
 };
 export function solution(roman) {
-  //string roman numeral into an integer
+  // Array of numbers that correspond to the Roman numeral
+  const arrOfNumbers = roman.split("").map((number) => romanNumeral[number]);
+  // Perform the reduction to calculate the decimal value
+  const result = arrOfNumbers.reduce(
+    (sum, currentNumber, index, arrOfNumbers) => {
+      // Check if the current number is less than the next number in the array to perform subtraction
+      if (currentNumber < arrOfNumbers[index + 1]) {
+        return sum - currentNumber;
+      }
+      return sum + currentNumber;
+    },
+    0
+  );
 
-  const romanArray = roman.split("");
-  let result = 0;
-  let prevValue = 0;
-  for (let char of romanArray) {
-    //all number that are corresponding to the character
-    const numericValue = romanNumeral[char];
-    //check if numeric value is  greater the prevValue
-    if (numericValue > prevValue) {
-      result += numericValue - 2 * prevValue;
-    } else {
-      result += numericValue;
-    }
-    prevValue = numericValue;
-  }
   return result;
 }
